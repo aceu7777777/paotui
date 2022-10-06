563d92def5d566e0dc7971957efe2e3f53696a1b
aceu7777777 <104356059+aceu7777777@users.noreply.github.com>
2022-10-06 21:25:53 +0800

    Update login_and_register.js

--- paopao-pc/src/store/login_and_register.js
+++ paopao-pc/src/store/login_and_register.js
@@ -1,6 +1,6 @@
 //登录和注册使用的仓库
 import {reqRegister,reqLogin} from '@/api'
-
+import store from '@/store'
 const state = {
     userID:'',//电话号码
 };
@@ -15,6 +15,7 @@ const actions = {
         let result = await reqLogin();
         if (result.code == 200 && result.data[0].userID == info.phone&&result.data[0].userPsd == info.pass) {
             commit("LOGIN",info.phone)
+            store.state.islogin = true
             return 'ok';
         } else {
             return Promise.reject(new Error('faile'));
